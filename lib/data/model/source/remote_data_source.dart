b5aee528946f94fb7ef7a64e0abc7fcace2deea9
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2023-07-25 20:38:46 +0700

    setup networking

--- lib/data/model/source/remote_data_source.dart
+++ lib/data/model/source/remote_data_source.dart
@@ -1,1 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:my_quran_id/data/model/general_model.dart';
 
+abstract class RemoteDataSource {
+  Future<GeneralModel<T>> getData<T>({
+    required String endPoint,
+    required T Function(dynamic) modelFromJson,
+  });
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final Dio _dio = Dio(BaseOptions(baseUrl: 'https://equran.id/api/v2'));
+
+  @override
+  Future<GeneralModel<T>> getData<T>({
+    required String endPoint,
+    required T Function(dynamic) modelFromJson,
+  }) async {
+    try {
+      final response = await _dio.get(endPoint);
+      return GeneralModel.fromJson(response.data, modelFromJson);
+    } on DioException catch (e) {
+      final errorMessage = e.response?.data['message'];
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
---
+++ lib/domain/quran_repository.dart
@@ -0,0 +1,19 @@
+import 'package:my_quran_id/data/model/general_model.dart';
+import 'package:my_quran_id/data/model/quran_model.dart';
+import 'package:my_quran_id/data/model/source/remote_data_source.dart';
+
+class QuranRepository {
+  final RemoteDataSource _dataSource;
+
+  QuranRepository(this._dataSource);
+
+  Future<GeneralModel<List<Quran>>> getListQuran() {
+    return _dataSource.getData(
+      endPoint: '/surat',
+      modelFromJson: (json) {
+        final List<dynamic> listSurah = json as List<dynamic>;
+        return listSurah.map((surah) => Quran.fromJson(surah)).toList();
+      },
+    );
+  }
+}
