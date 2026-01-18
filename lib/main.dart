ebdc1c6eefeb7752d6a2894fec2f6749f59c96be
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2026-01-18 20:34:44 +0700

    Remove Clarity Flutter integration and associated dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:clarity_flutter/clarity_flutter.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:my_quran_id/constant.dart';
 import 'package:my_quran_id/presentation/detail/cubit/last_read_cubit.dart';
@@ -14,21 +11,13 @@ List<String?> surahNames = [];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: 'assets/.env');
-  final config = ClarityConfig(
-    projectId: dotenv.env['CLARITY_ID'] ?? '',
-    logLevel: kDebugMode ? LogLevel.Debug : LogLevel.None,
-  );
   final lastReadCubit = LastReadCubit();
   await lastReadCubit.loadLastRead();
   initializeDateFormatting('id', null).then((_) {
     runApp(
-      ClarityWidget(
-        clarityConfig: config,
-        app: BlocProvider(
-          create: (context) => ScrollCubit(),
-          child: MyApp(lastReadCubit: lastReadCubit),
-        ),
+      BlocProvider(
+        create: (context) => ScrollCubit(),
+        child: MyApp(lastReadCubit: lastReadCubit),
       ),
     );
   });
